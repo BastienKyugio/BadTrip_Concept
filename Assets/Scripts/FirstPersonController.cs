5139757cfeb83ef20c4cd058663923e5e324c9e6
BastienKyugio <bastien.robidel12@gmail.com>
2025-06-05 18:39:51 +0200

    correction des deplacements

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float sprintMultiplier = 2f;
+
+    [Header("Look Parameters")]
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float upDownLookRange = 80f;
+
+    [Header("Reference")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private PlayerInputHandler playerInputHandler;
+
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+    private float CurrentSpeed => walkSpeed * (playerInputHandler.SprintTriggered ? sprintMultiplier: 1);
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private Vector3 CalculateWorldDirecion()
+    {
+        Vector3 inputDirection = new Vector3(playerInputHandler.MovementInput.x, 0f, playerInputHandler.MovementInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        return worldDirection.normalized;
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 worldDirection = CalculateWorldDirecion();
+        currentMovement.x = worldDirection.x * CurrentSpeed;
+        currentMovement.z = worldDirection.z * CurrentSpeed;
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    private void ApplyHorizontalRotation(float rotationAmount)
+    {
+        transform.Rotate(0, rotationAmount, 0);
+    }
+
+    private void ApplyVerticalRotation(float rotationAmount)
+    {
+        verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    private void HandleRotation()
+    {
+        float mouseXRotation = playerInputHandler.RotationInput.x * mouseSensitivity;
+        float mouseYRotation = playerInputHandler.RotationInput.y * mouseSensitivity;
+
+        ApplyHorizontalRotation(mouseXRotation);
+        ApplyVerticalRotation(mouseYRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [Header("Input Acton Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name Reference")]
+    [SerializeField] private string actionMapName = "Player";
+
+    [Header("Action Name Reference")]
+    [SerializeField] private string movement = "Movement";
+    [SerializeField] private string rotation = "Rotation";
+    [SerializeField] private string sprint = "Sprint";
+
+    private InputAction movementAction;
+    private InputAction rotationAction;
+    private InputAction sprintAction;
+
+    public Vector2 MovementInput { get; private set; }
+    public Vector2 RotationInput { get; private set ; }
+    public bool SprintTriggered { get; private set; }
+
+    private void Awake()
+    {
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
+        movementAction = playerControls.FindAction(movement);
+        rotationAction = playerControls.FindAction(rotation);
+        sprintAction = playerControls.FindAction(sprint);
+
+        SubscribeActionValuesToInputEvents();
+    }
+    private void SubscribeActionValuesToInputEvents()
+    {
+        movementAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
+        movementAction.canceled += inputInfo => MovementInput = Vector2.zero;
+
+        rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
+        rotationAction.canceled += inputInfo => RotationInput = Vector2.zero;
+
+        sprintAction.performed += inputInfo => SprintTriggered = true;
+        sprintAction.canceled += inputInfo => SprintTriggered = false;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.FindActionMap(actionMapName).Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.FindActionMap(actionMapName).Disable();
+    }
+}
